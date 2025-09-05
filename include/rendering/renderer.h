f2dbdd4ee71b76788889f2f0db12d1abf40ac01f
Yash <iyashj@protonmail.ch>
2025-09-05 20:09:42 +0300

    adds queueing draw calls; adds use of variant and visit to make extending rendering easier

--- include/rendering/renderer.h
+++ include/rendering/renderer.h
@@ -1,17 +1,44 @@
 #ifndef RENDERER_H
 #define RENDERER_H
+#include <expected>
+#include <memory>
+#include <variant>
 #include <string>
 #include "raylib.h"
-#include "irenderer.h"
+#include <vector>
+#include "../rendering/irenderer.h"
 
 namespace engine {
+
+    struct CircleParams {
+        Vector2 position;
+        float radius;
+        Color color;
+    };
+    struct TextureParams {
+        const Texture2D* texture;
+        Rectangle source;
+        Rectangle dest;
+        Vector2 origin;
+        float rotation;
+        Color tint;
+    };
+    struct TextParams {
+        std::string text;
+        Vector2 position;
+        int fontSize;
+        Color color;
+    };
+    using DrawCommand = std::variant<CircleParams, TextureParams, TextParams>;
+
     class Renderer : public IRenderer {
     public:
+        Renderer() = default;
         Renderer(const Renderer&) = delete;
         Renderer& operator=(const Renderer&) = delete;
         ~Renderer() override = default;
 
-        void initialise(int width, int height, const std::string& title) override;
+        [[nodiscard]] std::expected<void, std::string> initialize(int width, int height, const std::string& title) override;
         void shutdown() override;
 
         void beginFrame() override;
@@ -21,6 +48,10 @@ namespace engine {
         void setRenderTarget(RenderTexture2D& renderTarget) override;
         void clearRenderTarget() override;
 
+        // Queueing API
+        void queueDraw(const DrawCommand& cmd);
+
+        // These methods now queue draw commands
         void drawCircle(const Vector2& position, float radius, const Color& color) override;
         void drawTexture(const Texture2D& texture,
             const Rectangle& source,
@@ -39,11 +70,12 @@ namespace engine {
         [[nodiscard]] const Vector2 getScreenCenter() const override;
 
     private:
-        int windowWidth;
-        int windowHeight;
-        RenderTexture2D* renderTexture;
-        Vector2 screenCenter;
-        bool windowShouldClose;
+        int windowWidth = 0;
+        int windowHeight = 0;
+        std::unique_ptr<RenderTexture2D> renderTexture;
+        Vector2 screenCenter = {0, 0};
+        bool windowShouldClose = false;
+        std::vector<DrawCommand> drawQueue;
     };
 }
 
