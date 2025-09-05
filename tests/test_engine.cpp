3d525db46ba2127b3f4e03f0c5ab71e7d05b52e5
iyashj <yash@yousician.com>
2025-09-06 00:27:20 +0300

    fixes header and updates engine initialisation test name

--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -1,9 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
-#include "include/core/engine_core.h"
+#include "core/engine_core.h"
 
-TEST_CASE("EngineCore::init initialises the engine successfully", "[engine]")
+TEST_CASE("EngineCore::initialize initializes the engine successfully", "[engine]")
 {
-    auto& engienCore = EngineCore::getInstance();
+    auto& engineCore = engine::EngineCore::getInstance();
     REQUIRE(engineCore.initialize() == true);
     engineCore.shutdown();
 }
