91ef13b7f5012f2086024236c383313319828c89
iyashj <yash@yousician.com>
2025-09-05 20:16:14 +0300

    adds humble_engine for a single header library setup

---
+++ include/humble_engine.h
@@ -0,0 +1,9 @@
+#ifndef HUMBLE_ENGINE_H
+#define HUMBLE_ENGINE_H
+
+#include "core/engine_core.h"
+#include "logging/logger.h"
+#include "rendering/irenderer.h"
+#include "resources/resource_manager.h"
+
+#endif //HUMBLE_ENGINE_H
